91f1a3743e647dd975d63ba37f40b849b103ab5f
Aryan Singh <rajputaryan7575@gmail.com>
2026-06-30 17:05:29 +0530

    Add files via upload

---
+++ 30_june_prog1.py
@@ -0,0 +1,5 @@
+# WAP to break a Loop on n=5
+for n in range (1,11):
+    if (n==5):
+        pass
+    print(n)
---
+++ 30_june_prog2.py
@@ -0,0 +1,12 @@
+# Guess the number Puzzle
+n=7
+print("enter your number")
+a=int(input())
+
+if a < 7:
+    print("your number is less then number")
+elif a > 7:
+    print("your number is greater then number")
+else:
+    print("your numberis equal ")        
+
