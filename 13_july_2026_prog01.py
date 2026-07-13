00934f503ef9cf0fb2d14b6f3835eb0682eb247e
Aryan Singh <rajputaryan7575@gmail.com>
2026-07-13 11:06:42 +0530

    my today's work

---
+++ 13_july_2026_prog01.py
@@ -0,0 +1,10 @@
+# area of rectangle in def 
+def area_rectangle(length,width):
+    return length * width
+
+length= int(input())
+width= int(input())
+
+area = area_rectangle (length, width)
+
+print("area of rectangle",area)
