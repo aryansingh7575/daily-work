53d97ac38cabbb591828e7ecb9bb03a131e2b89e
Aryan Singh <rajputaryan7575@gmail.com>
2026-07-19 18:21:54 +0530

    today's work

---
+++ 19_july_2026_prog_01.py
@@ -0,0 +1,9 @@
+# write a function for division
+
+def div_numbers(a,b):
+    return a / b
+a=float(input("enter number:"))
+b=float(input("enter number: "))
+result = div_numbers(a,b)
+print(result)
+
